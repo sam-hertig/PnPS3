abbe92ec0f4e25eba7c9dd0e06f3b47f900c4a58
Sam Hertig <sam@samhertig.com>
2018-12-02 18:27:42 +0100

    Add audio files for Pfupf, start implementing playing of audio files

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,5 @@
-using System;
 using UnityEngine;
 
-//[Serializable]
 public class GameManager : MonoBehaviour {
 
       
--- Assets/Scripts/PfupfController.cs
+++ Assets/Scripts/PfupfController.cs
@@ -2,22 +2,47 @@
 
 public class PfupfController : BeingController
 {
+
+    public AudioClip[] repellingAudioClips; 
+    public AudioClip[] complainingAudioClips;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     protected override void Update ()
     {
         base.Update();
         if (numberOfBlankets == maxNumberOfBlankets) {
             gameManager.gameWon();
         }
+        // TO DO: complaining audio clips...
     }
-
 
     protected override void OnTriggerEnter (Collider other)
     {
         base.OnTriggerEnter(other);
         if (other.gameObject.CompareTag("Blanket") && numberOfBlankets < maxNumberOfBlankets && partnerController!=null)
         {
-        	other.gameObject.tag = "BlanketInPlace";
+            PlayAudio(true);
+            other.gameObject.tag = "BlanketInPlace";
             partnerController.numberOfBlankets--;
+            
+        }
+    }
+
+    private void PlayAudio(bool pookieCloseBy)
+    {
+        if (pookieCloseBy)
+        {
+            audioSource.PlayOneShot(repellingAudioClips[Random.Range(0, repellingAudioClips.Length)]);
+        }
+        else
+        {
+            audioSource.PlayOneShot(complainingAudioClips[Random.Range(0, complainingAudioClips.Length)]);
         }
     }
 }
