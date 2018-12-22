e83b5929272cfb903e6d549c3ef3b9780bbb5155
shertig <samuel.hertig@comerge.net>
2018-12-22 09:28:06 -0500

    Improve random positioning of blankets

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,10 +13,10 @@ public class GameManager : MonoBehaviour {
     {
         for (int i = 0; i < NumberOfBlankets; i++)
         {
-            int sign = 1 - 2*(i % 2);
-            float rand1 = Random.Range(sign * _blanketMinPosition, sign * _blanketMaxPosition);
-            float rand2 = Random.Range(sign * _blanketMinPosition, sign * _blanketMaxPosition);
-
+            int sign1 = 1 - 2 * (Mathf.RoundToInt(Random.Range(0f, 1f)));
+            int sign2 = 1 - 2 * (Mathf.RoundToInt(Random.Range(0f, 1f)));
+            float rand1 = Random.Range(sign1 * _blanketMinPosition, sign1 * _blanketMaxPosition);
+            float rand2 = Random.Range(sign2 * _blanketMinPosition, sign2 * _blanketMaxPosition);
             Vector3 randomPosition = new Vector3(rand1, 0.15f, rand2);
             GameObject blanket = Instantiate(BlanketPrefab, randomPosition, new Quaternion());
             SkinnedMeshRenderer blanketRenderer = blanket.GetComponent<SkinnedMeshRenderer>();
